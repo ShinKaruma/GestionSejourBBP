a4ddc57d871e2e000aab661b1e4674cb82dcb5ea
Charlyie <77623701+Contrebande69@users.noreply.github.com>
2021-11-22 08:46:31 +0100

    Fix des migrate

--- migrations/Version20211122074046.php
+++ migrations/Version20211122074046.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211119155000 extends AbstractMigration
+final class Version20211122074046 extends AbstractMigration
 {
     public function getDescription(): string
     {
